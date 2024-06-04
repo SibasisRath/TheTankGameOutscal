5bfb65dcfb09bca0870048f59e58532a5a301508
Sibasis Rath <sibasisrath2000@gmail.com>
2024-06-04 11:32:00 +0530

    Tank mvc set up done

---
+++ Assets/Scripts/TankModel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TankModel
+{
+    private TankController tankController;
+
+    public TankModel()
+    {
+
+    }
+
+    public void SetTankController(TankController tankController)
+    {
+        this.tankController = tankController;
+    }
+    
+}
---
+++ Assets/Scripts/TankSpawnerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawnerScript : MonoBehaviour
+{
+    [SerializeField] private TankView tankView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateTank();
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
+    }
+}
